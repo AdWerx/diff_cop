d359d2dbc042783a8fda16449c93ddf293b7627f
Josh Bielick <jbielick@gmail.com>
2016-03-10 19:08:58 -0500

    specs and coverage

--- spec/diff_cop_spec.rb
+++ spec/diff_cop_spec.rb
@@ -1,11 +1,68 @@
 require 'spec_helper'
 
 describe DiffCop do
+
+  subject { described_class.new }
+
   it 'has a version number' do
     expect(DiffCop::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context 'a single patch in a single file' do
+
+    before :example do
+      allow(subject).to receive(:diff_output).and_return(fixture('version.diff'))
+      allow(subject).to receive(:`).and_return(fixture('version.json'))
+    end
+
+    it 'calls rubocop only on the diffed file' do
+      expect(subject).to receive(:`).with(/version\.rb/)
+      subject.start
+    end
+
+    it 'prints the file and line numbers of offenses' do
+      expect { subject.start.print! }.to output(/lib\/diff_cop\/version\.rb:2:/).to_stdout
+    end
+
+    it 'prints the offenses' do
+      expect { subject.start.print! }.to output(/Operator `=` should be/).to_stdout
+    end
+
+    it 'returns false when there are offenses' do
+      subject.start
+      capture2 do
+        expect(subject.print!).to be_falsy
+      end
+    end
+
   end
+
+  context 'when there is no diff' do
+
+    before :example do
+      allow(subject).to receive(:diff_output).and_return(fixture('empty.diff'))
+    end
+
+    it 'does not invoke the linter' do
+      expect(subject).to_not receive(:`).with(/rubocop/)
+      expect(subject).to receive(:diff_output)
+      subject.start
+    end
+
+    describe '#print' do
+
+      it 'prints that all is well' do
+        expect { subject.start.print! }.to output(/no offenses/).to_stdout
+      end
+
+      it 'returns true' do
+        capture2 do
+          expect(subject.start.print!).to be_truthy
+        end
+      end
+
+    end
+
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,21 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'diffcop'
+
+require 'simplecov'
+
+SimpleCov.start
+
+require 'diff_cop'
+require 'pry'
+
+def fixture(name)
+  File.read(File.join(File.dirname(__FILE__), 'fixtures', name))
+end
+
+def capture2
+  stdout, stderr = $stdout, $stderr
+  $stdout, $sterr = StringIO.new, StringIO.new
+  yield
+  [stdout, stderr]
+ensure
+  $stdout, $stderr = stdout, stderr
+end
